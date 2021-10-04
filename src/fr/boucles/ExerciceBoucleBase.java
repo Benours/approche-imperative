26f7d9165111f29a219be22ecf33a488b0201b3b
benours <benjamin.baska@gmail.com>
2021-10-04 12:09:23 +0200

    tp

---
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -0,0 +1,23 @@
+package fr.boucles;
+
+public class ExerciceBoucleBase {
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.print(i + 1 + " ");
+		}
+		System.out.println();
+		for(int i = 0; i < 20; i++) {
+			System.out.print(i + 1 + "- BASKA Benjamin ");
+		}
+		System.out.println();
+		for(int i = 2; i < 101; i = i + 2) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		for(int i = 1; i < 100; i = i + 2) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+	}
+}
