df8734b7ce5f048366718379139a0abfb97deec6
benours <benjamin.baska@gmail.com>
2021-10-04 11:51:27 +0200

    ok

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,29 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+	public static void main(String[] args) {
+		byte b = 1;
+		short s = 1;
+		int i = 1;
+		long l = 1L;
+		float f = 1F;
+		double d = 1D;
+		char c = '1';
+		boolean bo = true;
+		String st = "1";
+		
+		System.out.print(b);
+		System.out.print(s);
+		System.out.print(i);
+		System.out.print(l);
+		System.out.print(f);
+		System.out.print(d);
+		System.out.print(c);
+		System.out.print(bo);
+		System.out.println(st);
+		
+		String randomString = "Voici le résultat d'un calcul :\n1 + 5 = 6";
+		
+		System.out.println(randomString);
+	}
+}
